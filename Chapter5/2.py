05f3de63b812eb6877d86f77a3e84f8336ecc029
Trương Tử Long <tulong15102006@gmail.com>
2025-11-18 15:52:31 +0700

    update

---
+++ Chapter5/2.py
@@ -0,0 +1,7 @@
+import numpy as np
+ 
+a = np.zeros((2,2)) # Khai báo mảng numpy 2 chiều 2x2 với các phần tử bằng 0
+b = np.ones((1,2))  # Khai báo mảng numpy 1 chiều với 2 phần tử bằng 1
+c = np.full((2,2), 7) # Khai báo mảng numpy 2 chiều 2x2 với các phần tử bằng 7
+d = np.eye(2)  # Khai báo mảng đơn vị 2x2
+e = np.random.random((2,2)) # Khai báo mảng numpy 2 chiều 2x2 với các phần tử ngẫu nhiên
